bd3f8fc7c866fbd4fc7054ce23aca00fa31e30b7
faraz vaissi <farazmrx@gmail.com>
2022-04-03 12:41:52 +0430

    Add some cool effects for scrolling to elements

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+    
+$('a[href*="#"]')
+
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) {
+          return false;
+        } else {
+          $target.attr('tabindex','-1');
+          $target.focus();
+        };
+      });
+    }
+  }
+  });
+});
