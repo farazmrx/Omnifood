a1fa27a733333b5df532d1deb9dacd818de321bf
faraz vaissi <farazmrx@gmail.com>
2022-10-08 10:18:00 +0330

    Implement a Sticky Navigation Bar

--- js/script.js
+++ js/script.js
@@ -4,6 +4,28 @@ document.querySelector('.btn__mobile--nav').addEventListener('click', function()
     document.querySelector('.header').classList.toggle('nav-open');
 });
 
+const sectionHeroEl = document.querySelector('.hero');
+
+const obs = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+  console.log(ent);
+  if (ent.isIntersecting === false) {
+    document.body.classList.add('sticky');
+  }
+  
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove('sticky');
+  }
+},
+{
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+obs.observe(sectionHeroEl);
+
+
+
 $('a[href*="#"]')
 
 .not('[href="#"]')
